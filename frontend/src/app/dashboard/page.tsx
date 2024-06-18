049789268272f56f6e2ae9161ea19038913d6751
gayashan4lk <gayashan4lk@gmail.com>
2024-06-18 16:56:59 +0530

    geting data

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,10 +1,46 @@
 import { TestForm } from '@/components/TestForm'
+import ExpensesDataGrid from '@/components/expenses-datagrid'
+import { RevenueChartSkeleton } from '@/components/skeletons'
+import { Suspense } from 'react'
 
-export default function Page() {
+export default async function Page() {
+	const res = await fetch(`${process.env.API_BASEURL}/expenses`)
+	const expenses = await res.json()
+	console.log(expenses)
+
 	return (
-		<div>
-			This is dashboard
-			<TestForm />
-		</div>
+		<main>
+			<h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+			<div className="mt-6">
+				<Suspense fallback={<RevenueChartSkeleton />}>
+					{/* <ExpensesDataGrid expenses={expenses} /> */}
+					<div className="flex gap-2">
+						<h6 className="">Date</h6>
+						<h6 className="">Payment Type</h6>
+						<h6 className="">Detail</h6>
+						<h6 className="">Paid In</h6>
+						<h6 className="">Paid Out</h6>
+					</div>
+
+					<div className="flex gap-2">
+						{expenses.map((item: any) => (
+							<div key={item._id}>
+								<h6 className="">{item.date}</h6>
+								<h6 className="">{item.payment_type}</h6>
+								<h6 className="">{item.detail}</h6>
+								<h6 className="">{item.is_paid ? item.payment : ''}</h6>
+								<h6 className="">{item.is_paid ? '' : item.payment}</h6>
+							</div>
+						))}
+					</div>
+				</Suspense>
+			</div>
+			{/* {expenses.map((item: any) => (
+				<div key={item._id}>{item.detail}</div>
+			))} */}
+
+			{/* This is dashboard
+			<TestForm /> */}
+		</main>
 	)
 }
