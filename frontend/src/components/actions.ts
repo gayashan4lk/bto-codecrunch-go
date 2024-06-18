21acd70567c04b90c70894b3042232f2420cd035
gayashan4lk <gayashan4lk@gmail.com>
2024-06-18 20:20:35 +0530

    try to fix build

--- frontend/src/components/actions.ts
+++ frontend/src/components/actions.ts
@@ -3,8 +3,9 @@ import { unstable_noStore as noStore } from 'next/cache'
 export async function fetchExpenses() {
 	noStore()
 	try {
-		const res = await fetch(`${process.env.API_BASEURL}/expenses`)
+		const res = await fetch(`${process.env.API_BASEURL}/expenses`, { method: 'get' })
 		const expenses = await res.json()
+		console.log(expenses)
 		if (!expenses) return []
 		return expenses
 	} catch (error) {
