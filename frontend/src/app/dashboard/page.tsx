f287b5883a8fce3f061e46da9e12a8963a1e38da
gayashan4lk <gayashan4lk@gmail.com>
2024-06-18 17:06:55 +0530

    view expenses

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -6,41 +6,39 @@ import { Suspense } from 'react'
 export default async function Page() {
 	const res = await fetch(`${process.env.API_BASEURL}/expenses`)
 	const expenses = await res.json()
-	console.log(expenses)
 
 	return (
 		<main>
 			<h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
 			<div className="mt-6">
 				<Suspense fallback={<RevenueChartSkeleton />}>
-					{/* <ExpensesDataGrid expenses={expenses} /> */}
-					<div className="flex gap-2">
-						<h6 className="">Date</h6>
-						<h6 className="">Payment Type</h6>
-						<h6 className="">Detail</h6>
-						<h6 className="">Paid In</h6>
-						<h6 className="">Paid Out</h6>
-					</div>
-
-					<div className="flex gap-2">
-						{expenses.map((item: any) => (
-							<div key={item._id}>
-								<h6 className="">{item.date}</h6>
-								<h6 className="">{item.payment_type}</h6>
-								<h6 className="">{item.detail}</h6>
-								<h6 className="">{item.is_paid ? item.payment : ''}</h6>
-								<h6 className="">{item.is_paid ? '' : item.payment}</h6>
-							</div>
-						))}
+					<div className="overflow-x-auto">
+						<table className="table table-zebra">
+							{/* head */}
+							<thead>
+								<tr>
+									<th>Date</th>
+									<th>Payment Type</th>
+									<th>Detail</th>
+									<th>Paid In</th>
+									<th>Paid Out</th>
+								</tr>
+							</thead>
+							<tbody>
+								{expenses.map((item: any) => (
+									<tr key={item._id}>
+										<td>{item.date}</td>
+										<td>{item.payment_type}</td>
+										<td>{item.detail}</td>
+										<td>{item.is_paid ? item.payment : ''}</td>
+										<td>{item.is_paid ? '' : item.payment}</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
 					</div>
 				</Suspense>
 			</div>
-			{/* {expenses.map((item: any) => (
-				<div key={item._id}>{item.detail}</div>
-			))} */}
-
-			{/* This is dashboard
-			<TestForm /> */}
 		</main>
 	)
 }
