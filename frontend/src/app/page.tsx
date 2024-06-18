3bd6effc9aaa98f2d023bc7917cb7f4ed171a154
gayashan4lk <gayashan4lk@gmail.com>
2024-06-18 15:19:39 +0530

    build placeholder form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,27 @@
 import Image from 'next/image'
 
 export default function Home() {
-	return <div>Hello</div>
+	async function create(formData: FormData) {
+		'use server'
+		console.log('formData', formData)
+		const fields = {
+			test: formData.get('test'),
+		}
+		console.log('fields', fields)
+
+		// Logic to mutate data...
+	}
+
+	return (
+		<div>
+			<h1>Basic form</h1>{' '}
+			<form action={create}>
+				<label htmlFor="test" className="mb-2 block text-sm font-medium">
+					Test
+				</label>
+				<input id="test" name="test" placeholder="Test" type="text" />
+				<button type="submit">Create</button>
+			</form>
+		</div>
+	)
 }
