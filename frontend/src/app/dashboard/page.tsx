c401d81c9be7603d0a20ffb7fe448c9b4ae3e864
gayashan4lk <gayashan4lk@gmail.com>
2024-06-18 19:59:32 +0530

    try fix build issue

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,41 +2,46 @@ import { RevenueChartSkeleton } from '@/components/skeletons'
 import { Suspense } from 'react'
 
 export default async function Page() {
-	const res = await fetch(`${process.env.API_BASEURL}/expenses`, { next: { revalidate: 10 } })
-	const expenses = await res.json()
-
 	return (
 		<main>
 			<h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
 			<div className="mt-6">
 				<Suspense fallback={<RevenueChartSkeleton />}>
-					<div className="overflow-x-auto">
-						<table className="table table-zebra">
-							{/* head */}
-							<thead>
-								<tr>
-									<th>Date</th>
-									<th>Payment Type</th>
-									<th>Detail</th>
-									<th>Paid In</th>
-									<th>Paid Out</th>
-								</tr>
-							</thead>
-							<tbody>
-								{expenses.map((item: any) => (
-									<tr key={item._id}>
-										<td>{item.date}</td>
-										<td>{item.payment_type}</td>
-										<td>{item.detail}</td>
-										<td>{item.is_paid ? item.payment : ''}</td>
-										<td>{item.is_paid ? '' : item.payment}</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-					</div>
+					<ExpensesList />
 				</Suspense>
 			</div>
 		</main>
+	)
+}
+
+async function ExpensesList() {
+	const res = await fetch(`${process.env.API_BASEURL}/expenses`, { next: { revalidate: 10 } })
+	const expenses = await res.json()
+	return (
+		<div className="overflow-x-auto">
+			<table className="table table-zebra">
+				{/* head */}
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Payment Type</th>
+						<th>Detail</th>
+						<th>Paid In</th>
+						<th>Paid Out</th>
+					</tr>
+				</thead>
+				<tbody>
+					{expenses.map((item: any) => (
+						<tr key={item._id}>
+							<td>{item.date}</td>
+							<td>{item.payment_type}</td>
+							<td>{item.detail}</td>
+							<td>{item.is_paid ? item.payment : ''}</td>
+							<td>{item.is_paid ? '' : item.payment}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	)
 }
