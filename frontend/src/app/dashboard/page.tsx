ecd94fd1afb03829c3ff444c88e7f3c4c182da13
gayashan4lk <gayashan4lk@gmail.com>
2024-06-18 18:47:16 +0530

    try to fix build issue

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -10,33 +10,39 @@ export default async function Page() {
 			<h1 className={`mb-4 text-xl md:text-2xl`}>Dashboard</h1>
 			<div className="mt-6">
 				<Suspense fallback={<RevenueChartSkeleton />}>
-					<div className="overflow-x-auto">
-						<table className="table table-zebra">
-							{/* head */}
-							<thead>
-								<tr>
-									<th>Date</th>
-									<th>Payment Type</th>
-									<th>Detail</th>
-									<th>Paid In</th>
-									<th>Paid Out</th>
-								</tr>
-							</thead>
-							<tbody>
-								{expenses.map((item: any) => (
-									<tr key={item._id}>
-										<td>{item.date}</td>
-										<td>{item.payment_type}</td>
-										<td>{item.detail}</td>
-										<td>{item.is_paid ? item.payment : ''}</td>
-										<td>{item.is_paid ? '' : item.payment}</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
-					</div>
+					{expenses && expenses.length !== 0 && <ExpensesDataTable expenses={expenses} />}
 				</Suspense>
 			</div>
 		</main>
+	)
+}
+
+function ExpensesDataTable({ expenses }: any) {
+	return (
+		<div className="overflow-x-auto">
+			<table className="table table-zebra">
+				{/* head */}
+				<thead>
+					<tr>
+						<th>Date</th>
+						<th>Payment Type</th>
+						<th>Detail</th>
+						<th>Paid In</th>
+						<th>Paid Out</th>
+					</tr>
+				</thead>
+				<tbody>
+					{expenses.map((item: any) => (
+						<tr key={item._id}>
+							<td>{item.date}</td>
+							<td>{item.payment_type}</td>
+							<td>{item.detail}</td>
+							<td>{item.is_paid ? item.payment : ''}</td>
+							<td>{item.is_paid ? '' : item.payment}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
 	)
 }
