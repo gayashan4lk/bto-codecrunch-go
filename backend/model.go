c1644d51315e65b0ddd3f61a9eff33054c664648
PasanGunathilaka <pasangunathilaka5@gmail.com>
2024-06-18 15:49:07 +0530

    Modified the model

--- backend/model.go
+++ backend/model.go
@@ -4,7 +4,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Expense struct
 type Expense struct {
-	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Amount float64            `json:"amount" bson:"amount"`
-	Note   string             `json:"note" bson:"note"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Date        string             `json:"date" bson:"date"`
+	PaymentType string             `json:"payment_type" bson:"payment_type"`
+	Detail      string             `json:"detail" bson:"detail"`
+	Payment     float64            `json:"payment" bson:"payment"`
 }
